6e8ec6a6f14f0a2ad471fdd9630d08622ab21675
Mauricio Velazquez <mauri_i9070@hotmail.com>
2024-06-01 21:52:31 -0300

    Layout - Creacion de Fragmentos (head, header y footer)

--- tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,13 +1,15 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("titulo", "Inicio");
         return "index";
     }
 }
