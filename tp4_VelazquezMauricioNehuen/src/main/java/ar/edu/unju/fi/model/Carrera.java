6f5c9f72a04c7f466d7080c9700dc0668b01db31
Mauricio Velazquez <mauri_i9070@hotmail.com>
2024-06-24 14:22:49 -0300

    Configuracion del proyecto para usar Lombok y se adaptaron todos los Models

--- tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,63 +1,18 @@
 package ar.edu.unju.fi.model;
 
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Carrera {
     private String codigo;
     private String nombre;
     private Byte cantidadAnios;
     private Boolean estado;
 
-    public Carrera() {
-    }
-
-    public Carrera(String codigo, String nombre, Byte cantidadAnios, Boolean estado) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.cantidadAnios = cantidadAnios;
-        this.estado = estado;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Byte getCantidadAnios() {
-        return cantidadAnios;
-    }
-
-    public void setCantidadAnios(Byte cantidadAnios) {
-        this.cantidadAnios = cantidadAnios;
-    }
-
-    public Boolean getEstado() {
-        return estado;
-    }
-
-    public void setEstado(Boolean estado) {
-        this.estado = estado;
-    }
-
-    @Override
-    public String toString() {
-        return "Carrera{" +
-                "codigo='" + codigo + '\'' +
-                ", nombre='" + nombre + '\'' +
-                ", cantidadAnios=" + cantidadAnios +
-                ", estado=" + estado +
-                '}';
-    }
 }
--- tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,8 +1,14 @@
 package ar.edu.unju.fi.model;
 
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Materia {
     private String nombre;
     private String codigo;
@@ -11,87 +17,4 @@ public class Materia {
     private String modalidad;
     private Docente docente;
     private Carrera carrera;
-
-    public Materia() {
-    }
-
-    public Materia(String nombre, String codigo, String curso, Byte cantidadHoras, String modalidad,
-                   Docente docente, Carrera carrera) {
-        this.nombre = nombre;
-        this.codigo = codigo;
-        this.curso = curso;
-        this.cantidadHoras = cantidadHoras;
-        this.modalidad = modalidad;
-        this.docente = docente;
-        this.carrera = carrera;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
-
-    public Byte getCantidadHoras() {
-        return cantidadHoras;
-    }
-
-    public void setCantidadHoras(Byte cantidadHoras) {
-        this.cantidadHoras = cantidadHoras;
-    }
-
-    public String getModalidad() {
-        return modalidad;
-    }
-
-    public void setModalidad(String modalidad) {
-        this.modalidad = modalidad;
-    }
-
-    public Docente getDocente() {
-        return docente;
-    }
-
-    public void setDocente(Docente docente) {
-        this.docente = docente;
-    }
-
-    public Carrera getCarrera() {
-        return carrera;
-    }
-
-    public void setCarrera(Carrera carrera) {
-        this.carrera = carrera;
-    }
-
-    @Override
-    public String toString() {
-        return "Materia{" +
-                "nombre='" + nombre + '\'' +
-                ", codigo='" + codigo + '\'' +
-                ", curso='" + curso + '\'' +
-                ", cantidadHoras=" + cantidadHoras +
-                ", modalidad='" + modalidad + '\'' +
-                ", docente=" + docente +
-                ", carrera=" + carrera +
-                '}';
-    }
 }
