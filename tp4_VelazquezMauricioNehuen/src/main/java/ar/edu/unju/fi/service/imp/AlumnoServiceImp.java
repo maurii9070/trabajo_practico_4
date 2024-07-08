a0cb93b8e61fd5d002ef32b20a12f2c17db68626
RocioAV <44351449@fi.unju.edu.ar>
2024-07-08 00:31:37 -0300

    Implementacion de trazas en AlumnoServiceImp

--- tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/service/imp/AlumnoServiceImp.java
+++ tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/service/imp/AlumnoServiceImp.java
@@ -3,6 +3,8 @@ package ar.edu.unju.fi.service.imp;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,9 @@ import ar.edu.unju.fi.service.IAlumnoService;
 
 @Service ("alumnoServiceMySql")
 public class AlumnoServiceImp implements IAlumnoService {
+	
+	private static Logger logger = LoggerFactory.getLogger(AlumnoServiceImp.class);
+	
 	 @Autowired
 	 private AlumnoRepository alumnoRepository;
 	 @Autowired 
@@ -21,47 +26,83 @@ public class AlumnoServiceImp implements IAlumnoService {
 
 	@Override
 	public List<AlumnoDTO> findAll() {
-		// TODO Auto-generated method stub
 		return alumnoMapper.toAlumnoDTOList(alumnoRepository.findAll());
+		
 	}
 
 	@Override
 	public AlumnoDTO findById(Long id) {
-		return alumnoMapper.toAlumnoDTO(alumnoRepository.findById(id).get());
+		AlumnoDTO alumno= alumnoMapper.toAlumnoDTO(alumnoRepository.findById(id).get());
+		if(alumno != null) {
+			logger.info("Alumno encontrado con exito");
+		}else {
+			logger.error("¡Alumno NO encontrado!");
+		}
+		return alumno;
 	}
 
 	@Override
 	public Alumno save(AlumnoDTO alumnoDTO) {
-		return alumnoRepository.save(alumnoMapper.toAlumno(alumnoDTO));
+		Alumno alumno=alumnoRepository.save(alumnoMapper.toAlumno(alumnoDTO));
+		if(alumno != null) {
+			logger.info("¡Alumno guardado exitosamente!");
+		}else {
+			logger.error("¡Alumno NO se pudo guardar!");
+		}
+		return alumno;
 	}
 
 	@Override
 	public void deleteById(Long id) {
 		Alumno alumno = alumnoRepository.findById(id).get();
-		alumno.setEstado(false);
-		alumnoRepository.save(alumno);
+		if(alumno != null) {
+			alumno.setEstado(false);
+			alumnoRepository.save(alumno);
+			logger.warn("Alumno ELIMINADO");
+		}else {
+			logger.error("Alumno NO se pudo eliminar");
+		}
+		
 		
 	}
 
 	@Override
 	public void edit(AlumnoDTO alumnoDTO) {
+		logger.info("¡Alumno modificado!");
 		alumnoRepository.save(alumnoMapper.toAlumno(alumnoDTO));
 		
 	}
 
 	@Override
 	public List<AlumnoDTO> findByCarrera(Long id) {
-		return alumnoMapper.toAlumnoDTOList(alumnoRepository.findByCarrera(id));
+		List<AlumnoDTO> alumnos=alumnoMapper.toAlumnoDTOList(alumnoRepository.findByCarrera(id));
+		if(alumnos.isEmpty()) {
+			logger.warn("No existen alumnos inscriptos en esta carrera");
+		}else {
+			logger.info("Alumnos inscriptos en esta carrera recuperados con exito");
+		}
+		return alumnos;
 	}
 
 	@Override
 	public List<AlumnoDTO> findByMateria(Long id) {
-		return alumnoMapper.toAlumnoDTOList(alumnoRepository.findByMateria(id));
+		List<AlumnoDTO> alumnos=alumnoMapper.toAlumnoDTOList(alumnoRepository.findByMateria(id));
+		if(alumnos.isEmpty()) {
+			logger.warn("No existen alumnos inscriptos en esta materia");
+		}else {
+			logger.info("Alumnos inscriptos en esta materia recuperados con exito");
+		}
+		return alumnos;
 	}
 
 	@Override
 	public List<AlumnoDTO> findByEstado(boolean estado) {
 		List<AlumnoDTO> alumnos= alumnoMapper.toAlumnoDTOList(alumnoRepository.findByEstado(estado));
+		if(alumnos.isEmpty()) {
+			logger.warn("No existen alumnos dados de alta");
+		}else {
+			logger.info("Alumnos activos recuperados con exito");
+		}
 		return alumnos;
 	}
 }
