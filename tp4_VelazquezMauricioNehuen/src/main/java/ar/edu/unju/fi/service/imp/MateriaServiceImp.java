40f8795309fe011cfa27c1d24f018984d2018757
RocioAV <44351449@fi.unju.edu.ar>
2024-07-08 00:56:14 -0300

    Implementacion de trazas en MateriaServiceImp

--- tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/service/imp/MateriaServiceImp.java
+++ tp4_VelazquezMauricioNehuen/src/main/java/ar/edu/unju/fi/service/imp/MateriaServiceImp.java
@@ -3,6 +3,8 @@ package ar.edu.unju.fi.service.imp;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import ar.edu.unju.fi.service.IMateriaService;
 
 @Service("materiaServiceMySql")
 public class MateriaServiceImp implements IMateriaService {
+	
+	private static Logger logger = LoggerFactory.getLogger(MateriaServiceImp.class);
 
 	@Autowired
 	 private MateriaRepository materiaRepository;
@@ -27,38 +31,69 @@ public class MateriaServiceImp implements IMateriaService {
 
 	@Override
 	public MateriaDTO findById(Long id) {
-		return materiaMapper.toMateriaDTO(materiaRepository.findById(id).get());
-		
+		MateriaDTO materiaDTO =materiaMapper.toMateriaDTO(materiaRepository.findById(id).get());
+		if (materiaDTO != null) {
+			logger.info("Materia encontrada con éxito: " + materiaDTO.getNombre());
+		} else {
+			logger.error("¡Materia NO encontrada!");
+		}
+		return materiaDTO;
 	}
 
 	@Override
 	public Materia save(MateriaDTO materiaDTO) {
-		return materiaRepository.save(materiaMapper.toMateria(materiaDTO));
-		
+		Materia materia = materiaRepository.save(materiaMapper.toMateria(materiaDTO));
+		if (materia != null) {
+			logger.info("¡Materia guardada exitosamente!");
+		} else {
+			logger.error("¡Materia NO se pudo guardar!");
+		}
+		return materia;
 	}
 
 	@Override
 	public void deleteById(Long id) {
 		Materia materia = materiaRepository.findById(id).get();
-		materia.setEstado(false);
-		materiaRepository.save(materia);
+		if (materia != null) {
+			materia.setEstado(false);
+			materiaRepository.save(materia);
+			logger.warn("Materia ELIMINADA (desactivada) con éxito.");
+		} else {
+			logger.error("¡Materia NO se pudo eliminar!");
+		}
 		
 	}
 
 	@Override
 	public void edit(MateriaDTO materiaDTO) {
-		materiaRepository.save(materiaMapper.toMateria(materiaDTO));
-		
+		Materia materia =materiaRepository.save(materiaMapper.toMateria(materiaDTO));
+		if (materia != null) {
+			logger.info("¡Materia modificada con éxito!");
+		} else {
+			logger.error("¡Materia NO se pudo modificar!");
+		}
 	}
 
 	@Override
 	public List<MateriaDTO> findByAlumno(Long id) {
-		return materiaMapper.toMateriaDTOList(materiaRepository.findByAlumnoId(id));
+		List<MateriaDTO> materiasDtos = materiaMapper.toMateriaDTOList(materiaRepository.findByAlumnoId(id));
+		if (materiasDtos.isEmpty()) {
+			logger.warn("No existen materias asignadas al alumno con ID: " + id);
+		} else {
+			logger.info("Materias asignadas al alumno con ID " + id + " recuperadas con éxito.");
+		}
+		return materiasDtos;
 	}
 
 	@Override
 	public List<MateriaDTO> findByEstado(boolean estado) {
 		List<MateriaDTO> materiasDtos =materiaMapper.toMateriaDTOList(materiaRepository.findByEstado(estado));
+		if (materiasDtos.isEmpty()) {
+			logger.warn("No existen materias con el estado especificado.");
+		} else {
+			logger.info("Materias con estado " + estado + " recuperadas con éxito.");
+		}
 		return materiasDtos;
+		
 	}
 }
